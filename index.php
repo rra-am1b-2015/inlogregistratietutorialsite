10ea44ba428eae4347b815fb352e5b8962b9070e
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-14 11:41:57 +0200

    Link toegvoegen

--- index.php
+++ index.php
@@ -2,67 +2,11 @@
 	
 ?>
 <!DOCTYPE html>
-<style>
-*
-{
-	margin:0px;
-	padding:0px;
-}
-
-body
-{
-	font-family:Verdana, Arial;
-}
-
-
-#container
-{
-	border:0px solid green;
-	width:60%;
-	//margin-top:300px;
-	//margin-left:100px;
-	margin:20px auto;
-	background-color: RGB(255,255,255);
-}
-
-#banner, #footer
-{
-	font-size:25px;
-	background-color:RGB(138,192,7);
-	color:white;
-	text-align:center;
-	padding:10px;
-	font-weight:bold;
-	
-}
-
-#banner
-{
-	border:1px solid green;
-	border-top-left-radius:12px;
-	border-top-right-radius:12px;
-}
-
-#content
-{
-	background-color: RGB(245,245,245);
-	border:1px solid RGB(138, 192, 7);
-	color:RGB(24, 24, 24);
-	padding:20px;
-	margin:20px 0px;
-}
-
-#footer
-{
-	border:1px solid yellow;
-	border-bottom-left-radius:12px;
-	border-bottom-right-radius:12px;
-}
-</style>
-
 <html>
- <head>
-	<title></title>
+ <head>	
+	<title>inlogregistratie - tutorialsite</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--<link rel="stylesheet" media="(max-width:800px)" type="text/css" href="css/style-mobile.css">-->
  </head>
  <body>
 	<div id="container">
@@ -72,7 +16,7 @@ body
 		<div id="content">
 			Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. Dit is wat content. 
 		</div>
-		<div id="footer">
+		<div id="footer" >
 			footer
 		</div>
 	</div>
