67ae9ca0e5e343b09ade0f797993b0920ad684f9
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-14 09:12:45 +0200

    div

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
 <?php
-	echo "Hallo wereld!";
+	
 ?>
+<!DOCTYPE html>
+<style>
+*
+{
+	margin:0px;
+	padding:0px;
+}
+
+div
+{
+	border:2px solid green;
+	width:60%;
+	//margin-top:300px;
+	//margin-left:100px;
+	margin:20px auto;
+}
+
+
+#container
+{
+	background-color: RGB(235,235,235);
+}
+
+#banner
+{
+	background-color: RGB(255,255,255);
+}
+</style>
+
+<html>
+ <head>
+	<title></title>
+ </head>
+ <body>
+	<div id="container">
+		<div id="banner">
+			Banner
+		</div>
+		<div>
+			Content
+		</div>
+		<div>
+			footer
+		</div>
+	</div>  
+ </body>
+</html>
+
