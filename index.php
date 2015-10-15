b71cea5c52509f8815f9ec239758c48178e8fc9e
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-15 11:48:08 +0200

    register

--- index.php
+++ index.php
@@ -17,13 +17,18 @@
 		<!--Maak hier een div voor de hyperlinks. Geef het divje een id="link"-->
 		<div id="link">
 			<a href="index.php?content=homepage">home</a>
-			<a href="index.php?content=test">test</a>
+			<a href="index.php?content=register_form">test</a>
+			<a href="index.php?content=test2">test2</a>
+			<a href="index.php?content=test3">test3</a>
 		</div>
 		
 		<div id="content">
 			<?php
-				$page = $_GET["content"];
-				include($page.".php"); ?>
+				if ( isset($_GET["content"]))
+				include($_GET["content"].".php");
+				else
+				include("homepage.php");
+			?>
 		</div>
 		<div id="footer" >
 			footer
