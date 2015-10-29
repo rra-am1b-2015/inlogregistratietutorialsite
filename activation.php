e641a637f3a5d5d7459c38855c7e52ecda78b612
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-29 10:41:25 +0100

    register

--- activation.php
+++ activation.php
@@ -1,3 +1,12 @@
 <?php
 	echo "Ik ga jou activeren...je id = ".$_GET["id"];
+	include("db_connect.php");
+	
+	$query = "UPDATE 	`users` 
+			  SET 		`activation` = 'true'
+			  WHERE 	`id` = ".$_GET["id"].";";
+			  
+	$result = mysqli_query($conn, $query);
+	
+	var_dump($result);
 ?>
--- register.php
+++ register.php
@@ -33,7 +33,7 @@
 			<head>
 			</head>
 			<body>
-				<h3 style='color:green; font-family:Verdana; font-size:60px;'>Bedankt voor het registreren. Door <a href='http://localhost/2015-2016/blok1/am1b/inlogregistratietutorialsite/index.php?content=activation&id=".$id."'>hier</a> te klikken activeert u het account en kunt u inloggen</h3>
+				<h3 style='color:grey; font-family:Verdana; font-size:12px;'>Bedankt voor het registreren. Door <a href='http://localhost/2015-2016/blok1/am1b/inlogregistratietutorialsite/index.php?content=activation&id=".$id."'>hier</a> te klikken activeert u het account en kunt u inloggen</h3>
 			</body>		
 		</html>";
 		$header = "From: Docent Applicatie en mediaontwikkeling Arjan de Ruijter <rra@mboutrecht.nl>\r\n";
