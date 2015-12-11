9f16da6053cc3f5efd483badcbf63785ac81ca4f
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-11 15:03:58 +0100

    slide

--- topic_links/jquery_links.php
+++ topic_links/jquery_links.php
@@ -1,3 +1,4 @@
 <a href='index.php?content=developer_homepage&topic=jquery&page=tutorials/jquery/get_started'>JQuery Get Started</a>
 <a href='index.php?content=developer_homepage&topic=jquery&page=tutorials/jquery/events'>JQuery Events</a>
 <a href='index.php?content=developer_homepage&topic=jquery&page=tutorials/jquery/fade'>JQuery Fade</a>
+<a href='index.php?content=developer_homepage&topic=jquery&page=tutorials/jquery/slide'>JQuery Slide</a>
--- tutorials/jquery/slide.php
+++ tutorials/jquery/slide.php
@@ -1,6 +1,6 @@
-<h4>JQuery Fade</h4>
+<h4>JQuery Slide</h4>
 <button id="btn_fade_in">FadeIn</button> 
-<button id="btn_fade_out">FadeOut</button> 
+<button id="btn_slide_up">Slide Up</button> 
 <button id="btn_fade_toggle">FadeToggleOut</button>
 <button id="btn_fade_to20">To20%</button>
 <button id="btn_fade_to100">To100%</button> 
@@ -25,11 +25,11 @@
 					   height : "200px",
 					   marginTop : "1em"};
 	
-		$("button[id^='btn_fade_']").css(btnCSS);
+		$("button[id^='btn_slide_']").css(btnCSS);
 		$("[id='fade_demo']").css(divCSS);
 		
-		$("[id$='out']").click(function(){
-			$("#fade_demo").fadeOut(2000, function(){
+		$("[id$='up']").click(function(){
+			$("#fade_demo").slideUp(2000, function(){
 				//alert("Hallo ik ben weg!!");
 				console.log($(this).text());
 				//$(this).css("visibility", "hidden").css("display", "block");
