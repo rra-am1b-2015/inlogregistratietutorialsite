cd19814cf48f29d6b0c0cd2de774fcda8d4fe291
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-14 14:33:42 +0100

    animation

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
  <head>	
 	<title>inlogregistratie - tutorialsite</title>
 	<link rel="stylesheet" type="text/css" href="css/style.php">
-	<link rel="stylesheet" media="(max-width:800px; )" type="text/css" href="css/style-mobile.css">
+	<link rel="stylesheet" type="text/css" href="./jquery/jquery-ui-1.11.4.custom/jquery-ui.css">
+ 	<link rel="stylesheet" media="(max-width:800px; )" type="text/css" href="css/style-mobile.css">
 	<script src="./jquery/jquery-2.1.4.js"></script>
+	<script src="./jquery/jquery-ui-1.11.4.custom/jquery-ui.js"></script>
 	<!--<script src="./jquery/jquery-2.1.4.min.js"></script>-->
 	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
  </head>
--- tutorials/jquery/animation.php
+++ tutorials/jquery/animation.php
@@ -11,6 +11,7 @@
 	$("document").ready(function(){
 		var divCSS = { border			:	"1px solid black",
 					   backgroundColor	:	"green",
+					   border			:	"10px solid orange",
 					   fontSize			:	"1.4em",
 					   padding			:	"0.5em",
 					   width			:	"100px",
@@ -21,23 +22,39 @@
 					   left				:	"0%"};
 		var animationTime = 500;
 		
-		var animationLeft = {left : "600px"};
+		var animationLeft = {left : "500px",
+							 fontSize : "3em",
+							 width: "200px",
+							 height: "200px",
+							 borderRadius: "2em",
+							 backgroundColor: "yellow"};
 		
-		var animationDown = {top : "250px"};
+		var animationDown = {top : "250px",
+							 width: "100px",
+							 height: "100px",
+							 fontSize : "1.4em",
+							 borderRadius: "0em",
+							 backgroundColor: "purple"};
 		
-		var animationRight = {left : "0px"};
+		var animationRight = {left : "0px",
+							  borderRadius: "2em",
+							  backgroundColor: "pink"};
 		
-		var animationUp = { top : "10px"};
+		var animationUp = { top : "10px",
+							borderRadius: "0em",
+							backgroundColor: "green"};
 		
+		$("#animation_demo").css(divCSS);
+		
+		$("[id^='btn_']").click(function(){
 		$("#animation_demo")
-			.css(divCSS)
 			.animate(animationLeft,
 					 3 * animationTime, 
 					 function(){ 
 						$(this).animate(animationDown,
 							animationTime, function() {
 								  $(this).animate(animationRight, 
-										animationTime, function(){
+										3 * animationTime, function(){
 											  $(this).animate(animationUp, animationTime, function(){
 																	
 																				 });
@@ -45,6 +62,10 @@
 											  });
 								  });									 
 					 });
+	});
+		
 										
 	});
+	
+	
 </script>
