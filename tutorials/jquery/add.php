aeccf3614d8d8755e6766b74acd3bca008afbd61
Arjan de Ruijter <rra@mboutrecht.nl>
2016-01-08 13:49:33 +0100

    add

--- tutorials/jquery/add.php
+++ tutorials/jquery/add.php
@@ -6,3 +6,29 @@
 	<li>Volkswagen</li>
 	<li>Ford</li>
 </ul>
+
+<button>Toevoegen extra tekst</button>
+<button id="after">Toevoegen extra tekst</button>
+
+<script>
+	$(document).ready(function(){
+		$("button").click(function(){
+			var auto = $("li").first();
+			auto.append(" mijn favoriete automerk");
+			auto.prepend("Als ik echt moet kiezen is ");
+
+			var listItems = $("li");
+			for ( var i =0; i < listItems.length; i++)
+			{
+				if ( listItems.eq(i).text() == "Ford")
+				{
+					listItems.eq(i).append(", is het beste automerk");
+				}
+			}
+		});
+		
+		
+	});
+</script>
+
+
