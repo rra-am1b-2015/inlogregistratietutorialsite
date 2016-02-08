c22fa64048fc2824f0d75ff4c3486be6596340fa
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-08 09:54:57 +0100

    op ssd laptop geinit

--- topic_links/javascript_links.php
+++ topic_links/javascript_links.php
@@ -2,5 +2,8 @@
 <a href='index.php?content=developer_homepage&topic=javascript&page=tutorials/javascript/get_started'>Javascript Get Started</a>
 <a href='index.php?content=developer_homepage&topic=javascript&page=tutorials/javascript/cookies'>Javascript Cookies</a>
 <a href='index.php?content=developer_homepage&topic=javascript&page=tutorials/javascript/arrays'>Javascript Array's</a>
+<a href='index.php?content=developer_homepage&topic=javascript&page=tutorials/javascript/objects'>Javascript Objects</a>
+<a href='index.php?content=developer_homepage&topic=javascript&page=tutorials/javascript/objects_prototype'>Javascript Objects Prototype</a>
+
 
 
---
+++ tutorials/javascript/objects_prototype.php
@@ -0,0 +1,1 @@
+<h4>Object prototype</h4>
