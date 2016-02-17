1bd2a118d1e73708acefeeba5f3ca6b484028d4d
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-17 11:13:34 +0100

    Select voorgedaan

--- tutorials/json/json_ajax_select.php
+++ tutorials/json/json_ajax_select.php
@@ -1,27 +1,36 @@
 <p><h4>Toepassing JSON string voor select tag</h4></p>
 
 <!-- Wanneer er op het select tag geclickt wordt, haalt het xmlhttp object alle voornamen asychroon
-	dus zonder de pagina te verversen binnen. Deze voornamen zijn te lezen in de select lijst. -->
+	 dus zonder de pagina te verversen binnen. Deze voornamen zijn te lezen in de select lijst. -->
 
-<select id="ajax_select">
-	<option>-- voornamen --</option>
-	<option>Arjan</option>
-	<option>Frans</option>
-	<option>Bert</option>
-</select>
+	<select id="ajax_select">
+		<option>-- voornamen --</option>
+		<option>Arjan</option>
+		<option>Frans</option>
+		<option>Bert</option>
+	</select>
 <script>
 
 
 
-document.getElementById("btn_JSON").onchange = function(){
-	// Maak een handvat op het select tag
+document.getElementById("ajax_select").onclick = function(){
+
+	 // Maak een handvat op het select tag	
 	 var xmlhttp = new XMLHttpRequest();
  
 	 xmlhttp.onreadystatechange = function(){
+		 alert(xmlhttp.readyState + " | " + xmlhttp.status);
 		 if ( xmlhttp.readyState == 4 && xmlhttp.status == 200)
 		 {
+			 //alert(xmlhttp.responseText);
 			 var jsObject = JSON.parse(xmlhttp.responseText);
-
+			 for ( var i = 0; i < jsObject.firstname.length; i++)
+			 {
+				alert(jsObject.firstname[i]);
+			 }
+			 
+			 
+			 /*
 		     var txtName = "Mijn naam is: " + jsObject.firstname + " " + jsObject.infix +  " " + jsObject.lastname + "<br>" +
 						   "Mijn leeftijd is: " + jsObject.leeftijd + "<br>";
 			 txtName += "Mijn favoriete kleuren zijn:<br>" 
@@ -32,13 +41,12 @@ document.getElementById("btn_JSON").onchange = function(){
 			 txtName += "Mijn leerlingnummer is: " + jsObject.opleiding.llnr + "<br>";
 			 txtName += "De naam van mijn opleiding is: " + jsObject.opleiding.naamOpleiding;
 
-			 document.getElementById("nameFromJSON").innerHTML = txtName;			 
+			 document.getElementById("nameFromJSON").innerHTML = txtName;
+			*/
 		 }	 
 	 }
-	 xmlhttp.open("POST", "http://localhost/am1b/inlogregistratietutorialsite-master/tutorials/json/json_data.php", true);
-	 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	 xmlhttp.send("id=4");
-	 
+	 xmlhttp.open("POST", "http://localhost/am1b/inlogregistratietutorialsite-master/tutorials/json/json_data_select.php", true);
+	 xmlhttp.send();	 
 };
 
 </script>
--- tutorials/json/json_data_select.php
+++ tutorials/json/json_data_select.php
@@ -1,11 +1,25 @@
 <?php
 	include("../../db_connect.php");
-	$query = "SELECT * FROM `users` WHERE `id` = '".$_POST['id']."'";	
+	$query = "SELECT `firstname` FROM `users`";	
 	$result = mysqli_query($conn, $query);	
-	$record = mysqli_fetch_assoc($result);
 	
+	$numberOfFirstnames = mysqli_num_rows($result);
+
+	$counter = 1;
+	$output = '{ "firstname" : [ ';
+	while ( $record = mysqli_fetch_assoc($result))
+	{
+		if ($counter < $numberOfFirstnames)
+		{
+			$output .= "\"".$record["firstname"]."\", ";
+		}
+		else
+		{
+			$output .= "\"".$record["firstname"]."\"";
+		}
+		$counter++;
+	}
+	$output .= ']}';
 	
-	$txt =  '{ "firstname" : "'.$record['firstname'].'", "infix" : "'.$record['infix'].'", "lastname" : "'.$record['lastname'].'", "leeftijd" : 47, "favorieteKleuren" : [ "geel", "groen", "blauw" ], "opleiding" : { "llnr" : "109321", "naamOpleiding" : "Applicatie- en Mediaontwikkeling" } }';
-	
-	echo $txt;
+	echo $output;
 ?>
